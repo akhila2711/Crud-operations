501cda64bc95530ba612eb51f2532eb0564ebb17
akhila2711 <akhilakhi112799@gmail.com>
2025-05-22 07:31:43 +0530

    add

--- app.js
+++ app.js
@@ -2,13 +2,21 @@ const express=require('express')
 const app=express()
 const bodyParser=require('body-parser')
 const ehbs=require('express-handlebars')//template engine
+const dbo=require('./db')
 
 app.engine('hbs',ehbs.engine({layoutsDir:'views',defaultLayout:"main",extname:"hbs"}))
 app.set("views","views")
 
-app.get('/',(req,res)=>{
-    let message="test"
+app.get('/',async (req,res)=>{
+    let database= await dbo.getdataBase()
+    const collection =database.collection("books")
+    const cursor=collection.find({})
+    let employee=cursor.toArray()
+    let message=" "
     res.render('main.hbs',{message})
 })
 
-app.listen(800)
+app.listen(8000,()=>{
+    console.log("listening to port 8000")
+
+})
--- db.js
+++ db.js
@@ -2,4 +2,14 @@ const db=require('mongodb')
 const mongoClient=db.mongoClient
 
 let database
-const client=mongoClient.connect()
+
+async function getdataBase(){
+    const client= await mongoClient.connect('mongodb://127.0.0.1:27017')
+    database=client.db('library')
+
+    if(!database){
+        console.log("database not found")
+    }
+    return database
+}
+module.exports=getdataBase()
