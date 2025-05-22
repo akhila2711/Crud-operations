dce13d5c0fb92c02e73e1ad1aeb4153583e590c0
akhila2711 <akhilakhi112799@gmail.com>
2025-05-22 07:10:10 +0530

    add

--- app.js
+++ app.js
@@ -6,4 +6,9 @@ const ehbs=require('express-handlebars')//template engine
 app.engine('hbs',ehbs.engine({layoutsDir:'views',defaultLayout:"main",extname:"hbs"}))
 app.set("views","views")
 
+app.get('/',(req,res)=>{
+    let message="test"
+    res.render('main.hbs',{message})
+})
+
 app.listen(800)
